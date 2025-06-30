e9beac9f2d1b6772348a258364a0e17333297e19
MaximZamotaev <112868632+MaximZamotaev@users.noreply.github.com>
2025-06-30 07:27:19 +0300

    StudentSatisfactionAnalyzer.cpp

--- StudentSatisfactionAnalyzer.cpp
+++ StudentSatisfactionAnalyzer.cpp
@@ -62,7 +62,7 @@ public:
         components = nullptr;
         // Initialize factorNames
         factorNames = gcnew array<String^> {
-                L"Эмоциональное состояние",
+            L"Эмоциональное состояние",
                 L"Психологическое состояние",
                 L"Физическое состояние",
                 L"Интеллектуальное состояние",
@@ -99,7 +99,7 @@ protected:
         if (components) {
             delete components;
         }
-        
+
         if (factorInputs) {
             for (int i = 0; i < factorInputs->Length; i++) {
                 if (factorInputs[i]) delete factorInputs[i];
@@ -145,7 +145,7 @@ private:
         this->Text = L"Оценка уровня удовлетворённости студентов";
         this->Size = Drawing::Size(1200, 750);
         this->StartPosition = FormStartPosition::CenterScreen;
-        this->BackColor = Color::FromArgb(255, 242, 237); 
+        this->BackColor = Color::FromArgb(255, 242, 237);
 
         // Create title label
         titleLabel = gcnew Label();
@@ -156,7 +156,7 @@ private:
         titleLabel->AutoSize = true;
         titleLabel->Size = Drawing::Size(1150, 40);
         titleLabel->TextAlign = ContentAlignment::MiddleCenter;
-        titleLabel->ForeColor = Color::FromArgb(205, 92, 92); 
+        titleLabel->ForeColor = Color::FromArgb(205, 92, 92);
         titleLabel->BackColor = Color::Transparent;
         this->Controls->Add(titleLabel);
 
@@ -166,7 +166,7 @@ private:
         factorsGroupBox->Location = Point(10, 80);
         factorsGroupBox->Size = Drawing::Size(1160, 380);
         factorsGroupBox->Font = gcnew Drawing::Font(factorsGroupBox->Font->FontFamily, 12, FontStyle::Bold);
-        factorsGroupBox->ForeColor = Color::FromArgb(205, 92, 92); 
+        factorsGroupBox->ForeColor = Color::FromArgb(205, 92, 92);
         factorsGroupBox->BackColor = Color::Transparent;
         this->Controls->Add(factorsGroupBox);
 
@@ -174,7 +174,7 @@ private:
         instructionsLabel1 = gcnew Label();
         if (!instructionsLabel1) throw gcnew Exception(L"Не удалось создать instructionsLabel1");
         instructionsLabel1->Text = L"Оцените по шкале от 0 до 10 включительно каждый из 20 факторов, влияющих на ваш уровень удовлетворённости обучением в высшем учебном заведении:";
-        instructionsLabel1->Font = gcnew Drawing::Font(instructionsLabel1->Font->FontFamily, 9, FontStyle::Bold);
+        instructionsLabel1->Font = gcnew Drawing::Font(instructionsLabel1->Font->FontFamily, 10.5, FontStyle::Bold);
         instructionsLabel1->Location = Point(10, 30);
         instructionsLabel1->Size = Drawing::Size(1140, 40);
         instructionsLabel1->ForeColor = Color::FromArgb(205, 92, 92);
@@ -188,9 +188,9 @@ private:
         if (!factorLabels) throw gcnew Exception(L"Не удалось создать массив factorLabels");
 
         int startX = 10;
-        int startY = 80;
-        int boxWidth = 70;
-        int boxHeight = 25;
+        int startY = 90;
+        int boxWidth = 71;
+        int boxHeight = 21;
         int labelWidth = 200;
         int spacing = 30;
         int itemsPerRow = 4;
@@ -204,8 +204,8 @@ private:
             factorLabels[i]->Text = factorNames[i];
             factorLabels[i]->Location = Point(startX + col * (labelWidth + boxWidth + 20),
                 startY + row * (boxHeight + spacing));
-            factorLabels[i]->Size = Drawing::Size(labelWidth, boxHeight * 2);
-            factorLabels[i]->Font = gcnew Drawing::Font(factorLabels[i]->Font->FontFamily, 9);
+            factorLabels[i]->Size = Drawing::Size(labelWidth * 1.05, boxHeight * 2.5);
+            factorLabels[i]->Font = gcnew Drawing::Font(factorLabels[i]->Font->FontFamily, 10, FontStyle::Bold);
             factorLabels[i]->ForeColor = Color::Black;
             factorLabels[i]->BackColor = Color::Transparent;
             factorsGroupBox->Controls->Add(factorLabels[i]);
@@ -214,9 +214,10 @@ private:
             if (!factorInputs[i]) throw gcnew Exception(String::Format(L"Не удалось создать factorInputs[{0}]", i));
             factorInputs[i]->Location = Point(startX + col * (labelWidth + boxWidth + 20) + labelWidth,
                 startY + row * (boxHeight + spacing));
-            factorInputs[i]->Size = Drawing::Size(boxWidth, boxHeight);
+            factorInputs[i]->Size = Drawing::Size(boxWidth, boxHeight * 1.5);
+            factorInputs[i]->TextAlign = HorizontalAlignment::Center;
             factorInputs[i]->TabIndex = i + 1;
-            factorInputs[i]->BackColor = Color::White; 
+            factorInputs[i]->BackColor = Color::White;
             factorInputs[i]->ForeColor = Color::Black;
             factorsGroupBox->Controls->Add(factorInputs[i]);
         }
@@ -225,9 +226,9 @@ private:
         GroupBox^ methodsGroupBox = gcnew GroupBox();
         methodsGroupBox->Text = L"Выбор метода";
         methodsGroupBox->Location = Point(10, 470);
-        methodsGroupBox->Size = Drawing::Size(520, 220);
+        methodsGroupBox->Size = Drawing::Size(520, 230);
         methodsGroupBox->Font = gcnew Drawing::Font(methodsGroupBox->Font->FontFamily, 12, FontStyle::Bold);
-        methodsGroupBox->ForeColor = Color::FromArgb(205, 92, 92); 
+        methodsGroupBox->ForeColor = Color::FromArgb(205, 92, 92);
         methodsGroupBox->BackColor = Color::Transparent;
         this->Controls->Add(methodsGroupBox);
 
@@ -235,10 +236,10 @@ private:
         instructionsLabel2 = gcnew Label();
         if (!instructionsLabel2) throw gcnew Exception(L"Не удалось создать instructionsLabel2");
         instructionsLabel2->Text = L"Выберите один из трёх методов машинного обучения:";
-        instructionsLabel2->Font = gcnew Drawing::Font(instructionsLabel2->Font->FontFamily, 9, FontStyle::Bold);
+        instructionsLabel2->Font = gcnew Drawing::Font(instructionsLabel2->Font->FontFamily, 10, FontStyle::Bold);
         instructionsLabel2->Location = Point(60, 30);
-        instructionsLabel2->Size = Drawing::Size(400, 20);
-        instructionsLabel2->ForeColor = Color::FromArgb(205, 92, 92); 
+        instructionsLabel2->Size = Drawing::Size(420, 20);
+        instructionsLabel2->ForeColor = Color::FromArgb(205, 92, 92);
         instructionsLabel2->BackColor = Color::Transparent;
         methodsGroupBox->Controls->Add(instructionsLabel2);
 
@@ -246,10 +247,10 @@ private:
         boostedTreesButton = gcnew Button();
         if (!boostedTreesButton) throw gcnew Exception(L"Не удалось создать boostedTreesButton");
         boostedTreesButton->Text = L"Boosted Trees";
-        boostedTreesButton->Location = Point(60, 60);
-        boostedTreesButton->Size = Drawing::Size(180, 30);
-        boostedTreesButton->Font = gcnew Drawing::Font(boostedTreesButton->Font->FontFamily, 9);
-        boostedTreesButton->BackColor = Color::FromArgb(233, 150, 122); 
+        boostedTreesButton->Location = Point(60, 55);
+        boostedTreesButton->Size = Drawing::Size(200, 30);
+        boostedTreesButton->Font = gcnew Drawing::Font(boostedTreesButton->Font->FontFamily, 9, FontStyle::Bold);
+        boostedTreesButton->BackColor = Color::FromArgb(233, 150, 122);
         boostedTreesButton->ForeColor = Color::Black;
         boostedTreesButton->FlatStyle = FlatStyle::Flat;
         boostedTreesButton->Click += gcnew EventHandler(this, &MainForm::BoostedTreesButton_Click);
@@ -258,10 +259,10 @@ private:
         randomForestButton = gcnew Button();
         if (!randomForestButton) throw gcnew Exception(L"Не удалось создать randomForestButton");
         randomForestButton->Text = L"Random Forest";
-        randomForestButton->Location = Point(260, 60);
-        randomForestButton->Size = Drawing::Size(180, 30);
-        randomForestButton->Font = gcnew Drawing::Font(randomForestButton->Font->FontFamily, 9);
-        randomForestButton->BackColor = Color::FromArgb(233, 150, 122); 
+        randomForestButton->Location = Point(280, 55);
+        randomForestButton->Size = Drawing::Size(200, 30);
+        randomForestButton->Font = gcnew Drawing::Font(randomForestButton->Font->FontFamily, 9, FontStyle::Bold);
+        randomForestButton->BackColor = Color::FromArgb(233, 150, 122);
         randomForestButton->ForeColor = Color::Black;
         randomForestButton->FlatStyle = FlatStyle::Flat;
         randomForestButton->Click += gcnew EventHandler(this, &MainForm::RandomForestButton_Click);
@@ -270,10 +271,10 @@ private:
         annButton = gcnew Button();
         if (!annButton) throw gcnew Exception(L"Не удалось создать annButton");
         annButton->Text = L"Искусственная нейронная сеть (ANN) с архитектурой MLP 20-17-3";
-        annButton->Location = Point(60, 100);
-        annButton->Size = Drawing::Size(380, 40);
-        annButton->Font = gcnew Drawing::Font(annButton->Font->FontFamily, 9);
-        annButton->BackColor = Color::FromArgb(233, 150, 122); 
+        annButton->Location = Point(60, 95);
+        annButton->Size = Drawing::Size(420, 40);
+        annButton->Font = gcnew Drawing::Font(annButton->Font->FontFamily, 9, FontStyle::Bold);
+        annButton->BackColor = Color::FromArgb(233, 150, 122);
         annButton->ForeColor = Color::Black;
         annButton->FlatStyle = FlatStyle::Flat;
         annButton->Click += gcnew EventHandler(this, &MainForm::ANNButton_Click);
@@ -283,20 +284,20 @@ private:
         instructionsLabel3 = gcnew Label();
         if (!instructionsLabel3) throw gcnew Exception(L"Не удалось создать instructionsLabel3");
         instructionsLabel3->Text = L"Либо же вы можете выбрать все три метода сразу для проведения более комплексного анализа:";
-        instructionsLabel3->Font = gcnew Drawing::Font(instructionsLabel3->Font->FontFamily, 9, FontStyle::Bold);
-        instructionsLabel3->Location = Point(60, 140);
-        instructionsLabel3->Size = Drawing::Size(400, 40);
-        instructionsLabel3->ForeColor = Color::FromArgb(205, 92, 92); 
+        instructionsLabel3->Font = gcnew Drawing::Font(instructionsLabel3->Font->FontFamily, 10, FontStyle::Bold);
+        instructionsLabel3->Location = Point(60, 145);
+        instructionsLabel3->Size = Drawing::Size(420, 40);
+        instructionsLabel3->ForeColor = Color::FromArgb(205, 92, 92);
         instructionsLabel3->BackColor = Color::Transparent;
         methodsGroupBox->Controls->Add(instructionsLabel3);
 
         allMethodsButton = gcnew Button();
         if (!allMethodsButton) throw gcnew Exception(L"Не удалось создать allMethodsButton");
         allMethodsButton->Text = L"Все три метода";
-        allMethodsButton->Location = Point(60, 180);
-        allMethodsButton->Size = Drawing::Size(380, 30);
-        allMethodsButton->Font = gcnew Drawing::Font(allMethodsButton->Font->FontFamily, 9);
-        allMethodsButton->BackColor = Color::FromArgb(233, 150, 122); 
+        allMethodsButton->Location = Point(60, 190);
+        allMethodsButton->Size = Drawing::Size(420, 30);
+        allMethodsButton->Font = gcnew Drawing::Font(allMethodsButton->Font->FontFamily, 9, FontStyle::Bold);
+        allMethodsButton->BackColor = Color::FromArgb(233, 150, 122);
         allMethodsButton->ForeColor = Color::Black;
         allMethodsButton->FlatStyle = FlatStyle::Flat;
         allMethodsButton->Click += gcnew EventHandler(this, &MainForm::AllMethodsButton_Click);
@@ -306,20 +307,21 @@ private:
         satisfactionLabel = gcnew Label();
         if (!satisfactionLabel) throw gcnew Exception(L"Не удалось создать satisfactionLabel");
         satisfactionLabel->Text = L"Уровень удовлетворённости:";
-        satisfactionLabel->Location = Point(550, 590);
+        satisfactionLabel->Location = Point(550, 605);
         satisfactionLabel->AutoSize = true;
-        satisfactionLabel->Font = gcnew Drawing::Font(satisfactionLabel->Font->FontFamily, 9);
+        satisfactionLabel->Font = gcnew Drawing::Font(satisfactionLabel->Font->FontFamily, 10, FontStyle::Bold);
         satisfactionLabel->ForeColor = Color::Black;
         satisfactionLabel->BackColor = Color::Transparent;
         this->Controls->Add(satisfactionLabel);
 
         satisfactionOutput = gcnew TextBox();
         if (!satisfactionOutput) throw gcnew Exception(L"Не удалось создать satisfactionOutput");
-        satisfactionOutput->Location = Point(750, 590);
-        satisfactionOutput->Size = Drawing::Size(350, 30);
+        satisfactionOutput->Location = Point(790, 605);
+        satisfactionOutput->Size = Drawing::Size(310, 30);
+        satisfactionOutput->TextAlign = HorizontalAlignment::Center;
         satisfactionOutput->Font = gcnew Drawing::Font(satisfactionOutput->Font->FontFamily, 10, FontStyle::Bold);
         satisfactionOutput->ReadOnly = true;
-        satisfactionOutput->BackColor = Color::White; 
+        satisfactionOutput->BackColor = Color::White;
         satisfactionOutput->ForeColor = Color::Black;
         this->Controls->Add(satisfactionOutput);
 
@@ -327,9 +329,9 @@ private:
         GroupBox^ resultsGroupBox = gcnew GroupBox();
         resultsGroupBox->Text = L"Результаты";
         resultsGroupBox->Location = Point(540, 470);
-        resultsGroupBox->Size = Drawing::Size(570, 220);
+        resultsGroupBox->Size = Drawing::Size(570, 230);
         resultsGroupBox->Font = gcnew Drawing::Font(resultsGroupBox->Font->FontFamily, 12, FontStyle::Bold);
-        resultsGroupBox->ForeColor = Color::FromArgb(205, 92, 92); 
+        resultsGroupBox->ForeColor = Color::FromArgb(205, 92, 92);
         resultsGroupBox->BackColor = Color::Transparent;
         this->Controls->Add(resultsGroupBox);
 
@@ -351,7 +353,7 @@ private:
         array<String^>^ methodNames = {
             L"Boosted Trees:",
             L"Random Forest:",
-            L"Искусственная нейронная сеть (ANN)\nс архитектурой MLP 20-17-3:"
+            L"Искусственная нейронная сеть (ANN) с архитектурой MLP 20-17-3:"
         };
 
         for (int i = 0; i < 3; i++) {
@@ -359,19 +361,20 @@ private:
             if (!methodLabels[i]) throw gcnew Exception(String::Format(L"Не удалось создать methodLabels[{0}]", i));
             methodLabels[i]->Text = methodNames[i];
             methodLabels[i]->Location = Point(10, i * 50);
-            methodLabels[i]->Size = Drawing::Size(250, 40);
-            methodLabels[i]->Font = gcnew Drawing::Font(methodLabels[i]->Font->FontFamily, 9);
+            methodLabels[i]->Size = Drawing::Size(300, 40);
+            methodLabels[i]->Font = gcnew Drawing::Font(methodLabels[i]->Font->FontFamily, 10, FontStyle::Bold);
             methodLabels[i]->ForeColor = Color::Black;
             methodLabels[i]->BackColor = Color::Transparent;
             combinedResultsPanel->Controls->Add(methodLabels[i]);
 
             satisfactionOutputs[i] = gcnew TextBox();
             if (!satisfactionOutputs[i]) throw gcnew Exception(String::Format(L"Не удалось создать satisfactionOutputs[{0}]", i));
-            satisfactionOutputs[i]->Location = Point(270, i * 50);
-            satisfactionOutputs[i]->Size = Drawing::Size(270, 30);
+            satisfactionOutputs[i]->Location = Point(320, i * 50);
+            satisfactionOutputs[i]->Size = Drawing::Size(220, 30);
+            satisfactionOutputs[i]->TextAlign = HorizontalAlignment::Center;
             satisfactionOutputs[i]->Font = gcnew Drawing::Font(satisfactionOutputs[i]->Font->FontFamily, 10, FontStyle::Bold);
             satisfactionOutputs[i]->ReadOnly = true;
-            satisfactionOutputs[i]->BackColor = Color::White; 
+            satisfactionOutputs[i]->BackColor = Color::White;
             satisfactionOutputs[i]->ForeColor = Color::Black;
             combinedResultsPanel->Controls->Add(satisfactionOutputs[i]);
         }
@@ -380,10 +383,10 @@ private:
         clearButton = gcnew Button();
         if (!clearButton) throw gcnew Exception(L"Не удалось создать clearButton");
         clearButton->Text = L"Очистить данные";
-        clearButton->Location = Point(10, 175);
+        clearButton->Location = Point(10, 190);
         clearButton->Size = Drawing::Size(550, 30);
-        clearButton->Font = gcnew Drawing::Font(clearButton->Font->FontFamily, 10);
-        clearButton->BackColor = Color::FromArgb(233, 150, 122); 
+        clearButton->Font = gcnew Drawing::Font(clearButton->Font->FontFamily, 10, FontStyle::Bold);
+        clearButton->BackColor = Color::FromArgb(233, 150, 122);
         clearButton->ForeColor = Color::Black;
         clearButton->FlatStyle = FlatStyle::Flat;
         clearButton->Click += gcnew EventHandler(this, &MainForm::ClearButton_Click);
@@ -493,15 +496,15 @@ protected:
 
     void ClearButton_Click(Object^ sender, EventArgs^ e) {
         try {
-            
+
             for (int i = 0; i < factorInputs->Length; i++) {
                 factorInputs[i]->Clear();
             }
 
-            
+
             satisfactionOutput->Clear();
 
-            
+
             if (satisfactionOutputs != nullptr) {
                 for (int i = 0; i < satisfactionOutputs->Length; i++) {
                     satisfactionOutputs[i]->Clear();
